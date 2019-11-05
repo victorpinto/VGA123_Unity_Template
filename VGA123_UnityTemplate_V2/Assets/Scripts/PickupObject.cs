297edb3da91a83ea13a35ad0447ed9884ae9ab28
victorpinto <mail@victorpinto.net>
2019-11-05 17:58:37 -0500

    Feature Update
    - added pickup object functionality
    - added drop box funcitonality
    - added button press functionality
    - added examples to Class template scene

---
+++ VGA123_UnityTemplate_V2/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    public Transform PickupPoint;
+    public Transform DropboxSpot;
+    //public Rigidbody rb;
+
+    public void Pickup()
+    {
+        gameObject.transform.parent = PickupPoint.transform;
+        transform.localPosition = Vector3.zero;
+        //rb.isKinematic = true;
+        Debug.Log("TriggeredParent");
+        //gameObject.SetActive (false); 
+    }
+
+    public void DropBox()
+    {
+        gameObject.transform.parent = DropboxSpot.transform;
+        transform.localPosition = Vector3.zero;
+        //rb.isKinematic = true;
+        Debug.Log("TriggeredDrop");
+        //gameObject.SetActive (false); 
+    }
+}
